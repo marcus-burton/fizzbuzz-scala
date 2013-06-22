98f95d742fc6e7312847132127df4be95cb39a51
Marcus Burton <marcus.mm.burton@gmail.com>
2013-06-22 14:57:22 -0400

    correcting divisibleBy3 and 5 functions

--- test/FizzBuzzTest.scala
+++ test/FizzBuzzTest.scala
@@ -14,13 +14,13 @@ class FizzBuzzTest extends FlatSpec with ShouldMatchers {
 
   it should "say number if not divisible by 3 or 5" in {
     val game = FizzBuzz(testGameLength)
-    for (i <- 1 to testGameLength filter (divisibleBy3(_)) filter (divisibleBy5(_)))
+    for (i <- 1 to testGameLength filter (!divisibleBy3(_)) filter (!divisibleBy5(_)))
       game(i - 1) should be(i.toString)
   }
 
-  def divisibleBy3(n: Int): Boolean = n % 3 != 0
+  def divisibleBy3(n: Int): Boolean = n % 3 == 0
 
-  def divisibleBy5(n: Int): Boolean = n % 5 != 0
+  def divisibleBy5(n: Int): Boolean = n % 5 == 0
 
   def randomGameLengthUpTo(n: Int): Int = nextInt(n)
 }
