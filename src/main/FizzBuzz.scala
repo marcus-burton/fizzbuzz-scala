5e8478b591b05588bf84fe9df958474ddbca4bd4
Marcus Burton <marcus.mm.burton@gmail.com>
2013-06-22 23:02:57 -0400

    Clean up impl. Reducing num of divisible checks

--- src/main/FizzBuzz.scala
+++ src/main/FizzBuzz.scala
@@ -1,20 +1,15 @@
-import scala.collection.mutable.ListBuffer
-
 object FizzBuzz {
-  def apply(n: Int): List[String] = {
-    val results = new ListBuffer[String]()
-    for (i <- 1 to n) {
-      i match {
-        case _ if divisibleBy3(i) && divisibleBy5(i) => results += "FizzBuzz"
-        case _ if divisibleBy3(i) => results += "Fizz"
-        case _ if divisibleBy5(i) => results += "Buzz"
-        case _ => results += i.toString
-      }
-    }
-    results.toList
+  def apply(gameLimit: Int): List[String] = {
+    (1 to gameLimit).map(fizzBuzzConversion).toList
   }
 
-  private def divisibleBy3(n: Int): Boolean = n % 3 == 0
+  private def fizzBuzzConversion(i: Int): String = i match {
+    case _ if divisibleBy3(i) => if (divisibleBy5(i)) "FizzBuzz" else "Fizz"
+    case _ if divisibleBy5(i) => "Buzz"
+    case _ => i.toString
+  }
 
-  private def divisibleBy5(n: Int): Boolean = n % 5 == 0
+  private def divisibleBy3(i: Int): Boolean = i % 3 == 0
+
+  private def divisibleBy5(i: Int): Boolean = i % 5 == 0
 }
